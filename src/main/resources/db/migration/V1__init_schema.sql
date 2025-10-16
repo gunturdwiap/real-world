5a2de0dd5308cbca23bcfb22e579100571a4dc48
gunturdwiap <kadekguntur13@gmail.com>
2025-10-16 14:18:00 +0800

    -amend

--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id CHAR(36) NOT NULL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     bio VARCHAR(255),
